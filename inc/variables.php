592cd296b459466721beab85f7755725fb0c5bad
independenciacn <admon@independenciacn.com>
2015-08-12 11:15:58 +0200

    Deshabilitador el reporte de errores

--- inc/variables.php
+++ inc/variables.php
@@ -14,7 +14,7 @@
  * @link     https://github.com/independenciacn/cni
  * @version  2.0e Estable
  */
-error_reporting(E_ALL ^ E_DEPRECATED);
+error_reporting(0);
 /**
  * Establecemos la zona horaria 
  */ 
